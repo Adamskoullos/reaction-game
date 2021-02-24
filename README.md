d3c4f59554bf2301dfdc564dccdc111064bac563
Adam <adamskoullos@gmail.com>
2021-02-24 18:04:43 +0000

    Reaction game functionality

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # reaction-game
 
+A very basic Vue starter project using three components. The tasks overview to complete the project are:
+
+1. Identify if someone is currently playing the game
+2. Start a random timer once each game begins to be reset at the end of each game
+3. Output each games results
+
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Game</h1>
+  <button @click="start" class="play" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @endGame="displayResults" />
+  <h1 v-if="showScore">Your reaction is {{ result }} ms</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      result: 0,
+      showScore: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random()*5000
+      this.isPlaying = true
+      this.showScore = false
+    },
+    displayResults(reactionTime){
+      this.result = reactionTime
+      this.isPlaying = false
+      this.showScore = true
+    }
   }
 }
 </script>
@@ -20,7 +39,17 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+.play{
+        width: 100px;
+        border-radius: 10px;
+        background: green;
+        color: #fff;
+        text-align: center;
+        padding: 20px;
+        margin: 40px auto;
+        font-size: 2rem;
+    }
 </style>
